3547f7b5d738ed9f3cde68489f26c8417b7311a9
Pablo Espitia <pabloespiti01@gmail.com>
2024-10-19 18:10:17 -0600

    primer comit

---
+++ script.js
@@ -0,0 +1,28 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.carousel-item');
+const totalSlides = slides.length;
+
+function showSlide(slideIndex) {
+    slides.forEach((slide, index) => {
+        slide.classList.remove('active');
+        if (index === slideIndex) {
+            slide.classList.add('active');
+        }
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % totalSlides;
+    showSlide(currentSlide);
+}
+
+function prevSlide() {
+    currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+    showSlide(currentSlide);
+}
+
+setInterval(() => {
+    nextSlide();
+}, 5000);
+
+showSlide(currentSlide);
